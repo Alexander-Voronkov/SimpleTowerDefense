82b78314678c4125dee1893824309c03addbb810
Voro_jn42@student.itstep.org <Voro_jn42@student.itstep.org>
2022-07-27 00:44:55 +0200

    almost final

--- TowerDefense/GamePage.xaml.cs
+++ TowerDefense/GamePage.xaml.cs
@@ -20,36 +20,191 @@ namespace TowerDefense
     /// </summary>
     public partial class GamePage : Page
     {
+        Random random = new Random();
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
         Frame frame;
         List<Point> points=new List<Point>();
+        int moneyCount = 100;
         public GamePage(Frame f)
         {
             InitializeComponent();
-            Background.ImageSource = (new Random().Next(0,10)%2==0)? new ImageSourceConverter().ConvertFromString("Textures/BackgroundObjects/Winter/Background.jpg") as ImageSource: new ImageSourceConverter().ConvertFromString("Textures/BackgroundObjects/Summer/Background.jpg") as ImageSource;
             frame = f;
-            StoneBrush.ImageSource = new ImageSourceConverter().ConvertFromString("Textures/stone.jpg") as ImageSource;
-            points.Add(new Point(500,900));
-            points.Add(new Point(500,300));
-            points.Add(new Point(900,300));
-            points.Add(new Point(900,900));
-            points.Add(new Point(1300,900));
-            points.Add(new Point(1300,100));
-            points.Add(new Point(0,100));            
+            Init();
+            
         }
 
-        private void Page_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Init()
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            PlanningMusic.Play();
+            Background.ImageSource = (new Random().Next(0, 10) % 2 == 0) ? new ImageSourceConverter().ConvertFromString("Textures/BackgroundObjects/Winter/Background.jpg") as ImageSource : new ImageSourceConverter().ConvertFromString("Textures/BackgroundObjects/Summer/Background.jpg") as ImageSource;
+            RandomBackObjectsGenerator();
+            StoneBrush.ImageSource = new ImageSourceConverter().ConvertFromString("Textures/stone.jpg") as ImageSource;
+            foreach (var item in (Path1.Data as PathGeometry).Figures)
             {
-                TowerControl tw = new TowerControl(new Dragon(), new Point(0, 900), points);
-                GameMap.Children.Add(tw);
-                tw.Move();
+                points.Add(item.StartPoint);
+                foreach (var item1 in item.Segments)
+                {
+                    points.AddRange((item1 as PolyLineSegment).Points);
+                }
             }
-            else if(e.RightButton==MouseButtonState.Pressed)
+            TowerControl inf = new TowerControl(new InfernalTower(), GameMap);
+            inf.TurnModifiersOff();
+            TowerControl can = new TowerControl(new CannonTower(), GameMap);
+            can.TurnModifiersOff();
+            TowerControl arc = new TowerControl(new ArcherTower(), GameMap);
+            arc.TurnModifiersOff();
+            TowerGrid.Children.Add(inf);
+            TowerGrid.Children.Add(can);
+            TowerGrid.Children.Add(arc);
+            Grid.SetRow(inf, 0);
+            Grid.SetRow(can, 1);
+            Grid.SetRow(arc, 2);
+            goldImg.Source = new ImageSourceConverter().ConvertFromString("Textures/money.png") as ImageSource;
+            heartsImg1.Source = new ImageSourceConverter().ConvertFromString("Textures/heart.png") as ImageSource;
+            heartsImg2.Source = new ImageSourceConverter().ConvertFromString("Textures/heart.png") as ImageSource;
+            heartsImg3.Source = new ImageSourceConverter().ConvertFromString("Textures/heart.png") as ImageSource;
+            goldCount.Content = moneyCount.ToString();
+
+
+            for (int j = 0; j < 1080; j += 150)
             {
-                TowerControl tw1 = new TowerControl(new Skeleton(), new Point(0, 900), points);
-                GameMap.Children.Add(tw1);
-                tw1.Move();
+                for (int i = 0; i < (1920 - (1920 / 100 * 20)); i += 150)
+                {
+                    Button b = new Button() { Opacity = 0.6, AllowDrop = true, Width = 150, Height = 150, Visibility = Visibility.Hidden, Background = Brushes.Transparent };
+                    GameMap.Children.Add(b);
+                    Canvas.SetLeft(b, i);
+                    Canvas.SetTop(b, j);
+                    b.DragOver += DragOverButton;
+                    b.DragLeave += DragLeaveButton;
+                    b.Drop += DragDropButton;
+                }
+            }
+            foreach (var item in GameMap.Children)
+            {
+                if (item is Button)
+                {
+                    for (int i = 0; i < points.Count - 1; i++)
+                    {
+                        if ((Canvas.GetTop(item as Button) - 30 >= points[i + 1].Y && Canvas.GetTop(item as Button) - 30 <= points[i].Y && Canvas.GetLeft(item as Button) - 30 >= points[i + 1].X && Canvas.GetLeft(item as Button) - 30 <= points[i].X))
+                        {
+                            (item as Button).Background = Brushes.PaleVioletRed;
+                            (item as Button).AllowDrop = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void RandomBackObjectsGenerator()
+        {
+            string path=null;
+            if (Background.ImageSource.ToString() == "Textures/BackgroundObjects/Summer/Background.jpg")
+            {
+                path = "Textures/BackgroundObjects/Summer/";
+            }
+            else
+                path = "Textures/BackgroundObjects/Winter/";
+            int q = random.Next(5,10);
+            for (int i = 0; i < q; i++)
+            {
+                string t=(random.Next()%2==0)?$"{path}1.png": $"{path}2.png";
+                Image img = new Image() { Width=150,Height=150};
+                GameMap.Children.Add(img);
+                img.Source = new ImageSourceConverter().ConvertFromString(t) as ImageSource;
+                int qqq = random.Next(0, 1080);
+                Canvas.SetTop(img,qqq);
+                int qqq1 = random.Next(0, 1500);
+                Canvas.SetLeft(img,qqq1);
+            }
+        }
+
+        private void DragOverButton(object sender, DragEventArgs d)
+        {
+            (sender as Button).Background = Brushes.LightGreen;
+        }
+        private void DragLeaveButton(object sender,DragEventArgs d)
+        {
+            (sender as Button).Background = Brushes.Transparent;
+        }
+        private void DragDropButton(object sender, DragEventArgs d)
+        {
+            (sender as Button).Background = Brushes.PaleVioletRed;
+            (sender as Button).AllowDrop = false;
+            foreach (var item in GameMap.Children)
+            {
+                if (item is Button)
+                {
+                    (item as Button).Visibility = Visibility.Hidden;
+                }
+            }
+            TowerControl newcontrol = (d.Data.GetData(typeof(TowerControl)) as TowerControl);
+            newcontrol.MakeUndroppable();
+            GameMap.Children.Add(newcontrol);
+            Canvas.SetLeft(newcontrol, Canvas.GetLeft(sender as Button));
+            Canvas.SetTop(newcontrol, Canvas.GetTop(sender as Button));
+        }
+
+        private void PlanningMusic_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            PlanningMusic.Stop();
+            PlanningMusic.Position = TimeSpan.MinValue;
+            PlanningMusic.Play();
+        }
+
+        private void StartGameBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PlayMusic.Position = TimeSpan.MinValue;
+            PlayMusic.Play();
+            PlanningMusic.Stop();
+            StartGameBtn.Content = "Stop";
+            StartGameBtn.Click -= StartGameBtn_Click;
+            StartGameBtn.Click += EndGameBtn_Click; 
+            RandomCharGenerator();
+        }
+        private void EndGameBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PlanningMusic.Position = TimeSpan.MinValue;
+            PlanningMusic.Play();
+            StartGameBtn.Content = "Start";
+            PlayMusic.Stop();
+            StartGameBtn.Click -= EndGameBtn_Click;
+            StartGameBtn.Click += StartGameBtn_Click;
+            ClearAttackers();
+        }
+        private void RandomCharGenerator()
+        {
+            int q = random.Next(3, 5);
+            for (int i = 0; i < q; i++)
+            {
+                if (random.Next(3,5) % 3 == 0)
+                {
+                    GameMap.Children.Add(new AttackerControl(new Zombie(),points[0],points));
+                }
+                else if(random.Next(3,5) % 4==0)
+                {
+                    GameMap.Children.Add(new AttackerControl(new Skeleton(), points[0], points));
+                }
+                else if (random.Next(3,5) % 5 == 0)
+                {
+                    GameMap.Children.Add(new AttackerControl(new Dragon(), points[0], points));
+                }
+            }
+            foreach (var item in GameMap.Children)
+            {
+                if (item is AttackerControl)
+                {
+                    (item as AttackerControl).Move();
+                }
+            }
+        }
+        private void ClearAttackers()
+        {
+            for (int i = 0; i < GameMap.Children.Count; i++)
+            {
+                if(GameMap.Children[i] is AttackerControl)
+                {
+                    GameMap.Children.Remove(GameMap.Children[i--]);
+                }
             }
         }
     }
--- TowerDefense/TowerControl.xaml.cs
+++ TowerDefense/TowerControl.xaml.cs
@@ -18,144 +18,98 @@ namespace TowerDefense
     /// <summary>
     /// Логика взаимодействия для TowerControl.xaml
     /// </summary>
-    /// 
-
     public partial class TowerControl : UserControl
     {
-        public IAttacker attacker;
-        public Point CurrentTarget;
-        private List<Point> Path;
-        private Image prev;
-        public TowerControl(IAttacker a, Point start ,  List<Point> path)
+        ITower tower;
+        Canvas buttons;
+        public void TurnModifiersOff()
+        {
+            this.MouseEnter -= UserControl_MouseEnter;
+            this.MouseLeave -= UserControl_MouseLeave;
+        }
+        public void MakeUndroppable()
+        {
+            this.MouseMove-= UserControl_MouseMove;
+        }
+        public TowerControl(ITower temp, Canvas b)
         {
             InitializeComponent();
-            Canvas.SetTop(this, start.Y);
-            Canvas.SetLeft(this, start.X);
-            attacker = a;
-            Path = path;
-            CurrentTarget = Path[0];
-            Init();
-            ChangeImage();
+            tower = temp;
+            buttons = b;
+            TowerImage.Source = tower.CurrentSprite.Source;
+            TowerName.Content = $"{tower.GetType().Name} Level {tower.Level}";
+            earthbrush.ImageSource = new ImageSourceConverter().ConvertFromString("Textures/earth.png") as ImageSource;
         }
-        private void Init()
+        public object Clone()
         {
-            CurrentHp.Value = attacker.Health;
-            CurrentHp.Maximum = attacker.Health;
-            CurrentHp.Minimum = 0;
-        }
-        private void Finish()
-        {
-            (this.Parent as Canvas).Children.Remove(this);
+            return this.MemberwiseClone();
         }
 
-        public async void Move()
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
-                while (true)
-                {
-                    if (Canvas.GetTop(this) < CurrentTarget.Y && Canvas.GetLeft(this) == CurrentTarget.X)
-                        Canvas.SetTop(this, Canvas.GetTop(this) + 1);
-                    else if (Canvas.GetTop(this) > CurrentTarget.Y && Canvas.GetLeft(this) == CurrentTarget.X)
-                        Canvas.SetTop(this, Canvas.GetTop(this) - 1);
-                    else if (Canvas.GetTop(this) == CurrentTarget.Y && Canvas.GetLeft(this) > CurrentTarget.X)
-                        Canvas.SetLeft(this, Canvas.GetLeft(this) - 1);
-                    else if (Canvas.GetTop(this) == CurrentTarget.Y && Canvas.GetLeft(this) < CurrentTarget.X)
-                        Canvas.SetLeft(this, Canvas.GetLeft(this) + 1);
-                    else if (Canvas.GetTop(this) == CurrentTarget.Y && Canvas.GetLeft(this) == CurrentTarget.X)
-                    {
-                        if (Path.IndexOf(CurrentTarget) + 1 != Path.Count)
-                            CurrentTarget = Path[Path.IndexOf(CurrentTarget) + 1];
-                        else
-                        {
-                            Finish();
-                            return;
-                        }
-                    }
-                    ChangeImage();
-                    await Task.Delay(TimeSpan.FromMilliseconds(attacker.Speed*10));
-                }
-        }
-
-        private void ChangeImage()
-        {
-            if (Sprite.Source != null)
+            if (this.Parent is Grid)
             {
-                    if (Canvas.GetTop(this) < CurrentTarget.Y && Canvas.GetLeft(this) == CurrentTarget.X)
+                (this.Parent as Grid).Children.Remove(this);
+            }
+            else if (this.Parent is Canvas)
+            {
+                Point p = new Point(Canvas.GetLeft(this),Canvas.GetTop(this));
+                (this.Parent as Canvas).Children.Remove(this);
+                foreach (var item in buttons.Children)
+                {
+                    if(item is Button&&p==new Point(Canvas.GetLeft(item as Button),Canvas.GetTop(item as Button)))
                     {
-                        if (attacker.Sprites.IndexOf(prev) < 2)
-                        {
-                            prev = (attacker).Sprites[attacker.Sprites.IndexOf(prev) + 1];
-                            Sprite.Source = (attacker).Sprites[(attacker).Sprites.IndexOf(prev) ].Source;
-                        }
-                        else if ((attacker).Sprites.IndexOf(prev) >= 2)
-                        {
-                            prev = (attacker).Sprites[0];
-                            Sprite.Source = (attacker).Sprites[0].Source;
-                        }
-                    }
-                    else if (Canvas.GetTop(this) > CurrentTarget.Y && Canvas.GetLeft(this) == CurrentTarget.X)
-                    {
-                        if ((attacker).Sprites.IndexOf(prev) < 5)
-                        {
-                            prev = (attacker).Sprites[(attacker).Sprites.IndexOf(prev) + 1];
-                            Sprite.Source = (attacker).Sprites[(attacker).Sprites.IndexOf(prev) ].Source;
-                        }
-                        else if ((attacker).Sprites.IndexOf(prev) >= 5)
-                        {
-                            prev = (attacker).Sprites[3];
-                            Sprite.Source = (attacker).Sprites[3].Source;
-                        }
-                    }
-                    else if (Canvas.GetTop(this) == CurrentTarget.Y && Canvas.GetLeft(this) < CurrentTarget.X)
-                    {
-                        if ((attacker).Sprites.IndexOf(prev) < 11)
-                        {
-                            prev = (attacker).Sprites[(attacker).Sprites.IndexOf(prev) + 1];
-                            Sprite.Source = (attacker).Sprites[(attacker).Sprites.IndexOf(prev) ].Source;
-                        }
-                        else if ((attacker).Sprites.IndexOf(prev) >= 11)
-                        {
-                            prev = (attacker).Sprites[9];
-                            Sprite.Source = (attacker).Sprites[9].Source;
-                        }
-                    }
-                    else if (Canvas.GetTop(this) == CurrentTarget.Y && Canvas.GetLeft(this) > CurrentTarget.X)
-                    {
-                        if ((attacker).Sprites.IndexOf(prev) < 8)
-                        {
-                            prev = (attacker).Sprites[(attacker).Sprites.IndexOf(prev) + 1];
-                            Sprite.Source = (attacker).Sprites[(attacker).Sprites.IndexOf(prev) ].Source;
-                        }
-                        else if ((attacker).Sprites.IndexOf(prev) >= 8)
-                        {
-                            prev = (attacker).Sprites[6];
-                            Sprite.Source = (attacker).Sprites[6].Source;
-
-                        }
+                        (item as Button).Background = Brushes.Transparent;
+                        (item as Button).AllowDrop = true;
                     }
                 }
+            }            
+        }
+
+        private void UpgradeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tower.Level < 3)
+            {
+                this.tower.Level = (short)(tower.Level + 1);
+                TowerImage.Source = tower.CurrentSprite.Source;
+                TowerName.Content = $"{tower.GetType().Name} Level {tower.Level}";
+            }
             else
             {
-                    if (Canvas.GetTop(this) < CurrentTarget.Y && Canvas.GetLeft(this) == CurrentTarget.X)
-                    {
-                            prev = (attacker).Sprites[0];
-                            Sprite.Source = (attacker).Sprites[0].Source;
-                    }
-                    else if (Canvas.GetTop(this) > CurrentTarget.Y && Canvas.GetLeft(this) == CurrentTarget.X)
-                    {
-                        prev = (attacker).Sprites[3];
-                            Sprite.Source = (attacker).Sprites[3].Source;
-                    }
-                    else if (Canvas.GetTop(this) == CurrentTarget.Y && Canvas.GetLeft(this) < CurrentTarget.X)
-                    {
-                        prev = (attacker).Sprites[9];
-                            Sprite.Source = (attacker).Sprites[9].Source;
-                    }
-                    else if (Canvas.GetTop(this) == CurrentTarget.Y && Canvas.GetLeft(this) > CurrentTarget.X)
-                    {
-                            prev = (attacker).Sprites[6];
-                            Sprite.Source = (attacker).Sprites[6].Source;
-                    }
-                }
+                UpgradeButton.IsEnabled = false;
             }
         }
+
+        private void UserControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                foreach (var item in buttons.Children)
+                {
+                    if (item is Button)
+                        ((Button)item).Visibility = Visibility.Visible;
+                }                
+                if(tower is InfernalTower)
+                    DragDrop.DoDragDrop(this, new TowerControl(new InfernalTower(), buttons) { Width = 150, Height = 150 }, DragDropEffects.Copy);
+                else if(tower is ArcherTower)
+                    DragDrop.DoDragDrop(this, new TowerControl(new ArcherTower(), buttons) { Width = 150, Height = 150 }, DragDropEffects.Copy);
+                else if (tower is CannonTower)
+                    DragDrop.DoDragDrop(this, new TowerControl(new CannonTower(), buttons) { Width=150,Height=150}, DragDropEffects.Copy);
+            }
+        }
+
+        private void UserControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UpgradeButton.Visibility = Visibility.Visible;
+            RemoveButton.Visibility = Visibility.Visible;
+            TowerName.Visibility = Visibility.Visible;
+        }
+
+        private void UserControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            UpgradeButton.Visibility = Visibility.Hidden;
+            RemoveButton.Visibility = Visibility.Hidden;
+            TowerName.Visibility = Visibility.Hidden;
+        }
     }
+}
